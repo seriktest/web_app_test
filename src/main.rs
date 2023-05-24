be376817c9237393d3b8608724656169fd77ac49
serik <podelu000@gmail.com>
2023-05-24 20:29:16 +0300

    add helth_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use actix_web::{web, App, HttpServer, Responder, HttpRequest};
+use actix_web::{web, App, HttpServer, Responder, HttpRequest, HttpResponse};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", name)
 }
+async fn health_check() -> impl Responder { HttpResponse::Ok() }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -11,6 +12,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
     })
         .bind("127.0.0.1:8000")?
         .run()
